73485e0f16a1b4b048463e5fff4e47c109cabab3
Emerson De Farias Santos <emerson.farias@unimedjp.com.br>
2024-08-04 14:21:40 -0300

    create projetc initail

---
+++ dio-first-step/src/test/java/dio/diofirststep/DioFirstStepApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.diofirststep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioFirstStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
